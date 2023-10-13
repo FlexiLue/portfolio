c8bf699a0bad89ac21a28cb2ad88587b767de8c2
Felix <felix.luette@web.de>
2023-10-13 12:31:40 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./content/*.md", "./layouts/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ['Inter']
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1024px',
+        '2xl': '1024px',
+      }
+    },
+  },
+  plugins: [],
+}
+
